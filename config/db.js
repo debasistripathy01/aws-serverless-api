52a90099a3b752572714c8c7016ba27ef29fa631
debasistripathy01 <debasistripathy04@gmail.com>
2023-07-07 15:54:32 +0530

    first commit

---
+++ config/db.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+
+const connection = mongoose.connect(process.env.mongoURL);
+module.exports=connection;
